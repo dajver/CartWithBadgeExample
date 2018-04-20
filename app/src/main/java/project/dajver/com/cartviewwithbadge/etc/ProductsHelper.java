b76449786d604f5f621fd28a00e54b9b45267460
gleb <lfqdth5@gmail.com>
2018-04-20 13:49:47 +0300

    added example of app changed links of pictures to github

--- app/src/main/java/project/dajver/com/cartviewwithbadge/etc/ProductsHelper.java
+++ app/src/main/java/project/dajver/com/cartviewwithbadge/etc/ProductsHelper.java
@@ -13,7 +13,7 @@ public class ProductsHelper {
         model.setId(5678l);
         model.setTitle("Honor 6A 2");
         model.setDescription("16Gb Grey");
-        model.setImage("https://www.citrus.ua/upload/new_iblock/100/c9f1994b1520c4f05ac9c1f7bd5690e0.jpg");
+        model.setImage("https://github.com/dajver/CartWithBadgeExample/blob/master/images/honor.jpg?raw=true");
         model.setPrice(599);
         productModels.add(model);
 
@@ -21,7 +21,7 @@ public class ProductsHelper {
         model.setId(5672l);
         model.setTitle("Meizu M5s");
         model.setDescription("32Gb Silver");
-        model.setImage("https://cdn1.citrus.ua/upload/new_iblock/5e1/a832e835d186c8156f0090a1319e2a68.jpg");
+        model.setImage("https://github.com/dajver/CartWithBadgeExample/blob/master/images/meizu.jpg?raw=true");
         model.setPrice(899);
         productModels.add(model);
 
@@ -29,14 +29,14 @@ public class ProductsHelper {
         model.setId(5673l);
         model.setTitle("Apple iPhone SE");
         model.setDescription("32Gb Space Gray");
-        model.setImage("https://www.citrus.ua/upload/new_iblock/ee3/266368eb03ab58074c8c0d6947f560a7.jpg");
+        model.setImage("https://github.com/dajver/CartWithBadgeExample/blob/master/images/iphone.jpg?raw=true");
         model.setPrice(1199);
         productModels.add(model);
 
         model = new ProductModel();
         model.setId(5674l);
         model.setTitle("Chuwi Hi10 Pro");
-        model.setImage("https://cdn1.citrus.ua/upload/new_iblock/fe8/c13bbeaa7cc8ac126cf96dd12d07d36c.jpg");
+        model.setImage("https://github.com/dajver/CartWithBadgeExample/blob/master/images/chuwi.jpg?raw=true");
         model.setPrice(2199);
         productModels.add(model);
 
@@ -44,7 +44,7 @@ public class ProductsHelper {
         model.setId(5675l);
         model.setTitle("Fermi S7-plus");
         model.setDescription("10000mAh gray)");
-        model.setImage("https://www.citrus.ua/upload/new_iblock/f90/9713854e2db57da9e40343b0ca65a40a.jpg");
+        model.setImage("https://github.com/dajver/CartWithBadgeExample/blob/master/images/batary.jpg?raw=true");
         model.setPrice(259);
         productModels.add(model);
 
